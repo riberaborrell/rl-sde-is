e7243a34a197df4fa2a4452bf904d7dba6300197
Enric Ribera <ribera.borrell@fu-berlin.de>
2024-10-06 16:42:34 +0200

    minor

--- src/rl_sde_is/dpg/eval_model_based_dpg_ol.py
+++ src/rl_sde_is/dpg/eval_model_based_dpg_ol.py
@@ -30,7 +30,6 @@ def main():
     # load model based dpg
     data = model_based_dpg(
         env,
-        return_type=args.return_type,
         gamma=args.gamma,
         n_layers=args.n_layers,
         d_hidden_layer=args.d_hidden,
