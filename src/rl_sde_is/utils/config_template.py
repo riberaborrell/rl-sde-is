3d5bde912314062dc9f0e35c3b742ca52db76b33
Enric Ribera <ribera.borrell@fu-berlin.de>
2024-12-08 20:36:12 +0100

    config template updated

--- src/rl_sde_is/utils/config_template.py
+++ src/rl_sde_is/utils/config_template.py
@@ -5,3 +5,6 @@ PROJECT_ROOT_DIR = '/project/abs/path'
 
 # set the absolute path of the data directory
 DATA_ROOT_DIR = '/data/abs/path'
+
+# set the absolute path of the output directory
+OUTPUT_ROOT_DIR = '/data/abs/path'
